9b94755d22ee694ff116ab9754226a67d6fbadbc
Tetiana Siestova <siestova.t@gmail.com>
2023-05-19 16:12:15 +0300

    map added

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -7,35 +7,47 @@ import Button from "./button/Button";
 import locationStyles from "./location.module.scss";
 import Icon from "./button/Icon"
 import IconRight from "./button/icon-arrow-left.svg"
-
+import Map from "./map/Map"
+import layoutStyles from "../components/layout.module.scss";
 
 const Location = () => {
 
   return (
     <div className={locationStyles.location__page__wrap}>
-      <div className={locationStyles.layout}>
-        <Link to="/">
-          {HomePage}
+      <div className={layoutStyles.layout}>
+          <div className={locationStyles.btn__wrap}>
+              <Link to="/">
+                  <Button type="button"
+                          iconLeft={ <Icon>
+                              <img src={IconRight} alt="" />
+                          </Icon>}
+                  >
+                      BACK TO HOME
+                  </Button>
 
-          <Button type="button"
-                  iconLeft={ <Icon>
-                    <img src={IconRight} alt="" />
-                  </Icon>}
-          >
-            BACK TO HOME
-          </Button>
-
-        </Link>
+              </Link>
+          </div>
       </div>
+        <Map />
       <div className={locationStyles.location__section__wrap}>
         <section className={locationStyles.location__section}>
           <div className={locationStyles.layout}>
             <h1 className={locationStyles.heading}>OUR LOCATION</h1>
-            <div>
-              Our newly opened gallery is located near the Edward King House on
-              99 King Street, the Modern Art Gallery is free to all visitors and
-              open seven days a week from 8am to 9pm.
-            </div>
+              <div>
+                  <div className={locationStyles.address}>99 King Street</div>
+                  <div className={locationStyles.address__description}>
+                      <span>Newport</span> <span>RI 0284</span>
+                      <span>United States of America</span>
+                  </div>
+                  <div className={locationStyles.address__description}>
+                      Our newly opened gallery is located near the Edward King House on 99 King Street, the Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm.
+                  </div>
+                  <div>
+                      Our newly opened gallery is located near the Edward King House on
+                      99 King Street, the Modern Art Gallery is free to all visitors and
+                      open seven days a week from 8am to 9pm.
+                  </div>
+              </div>
           </div>
         </section>
       </div>
---
+++ src/components/map/Map.jsx
@@ -0,0 +1,71 @@
+import React, {useEffect, useRef} from 'react';
+import mapStyles from "./map.module.scss"
+import icon from "./icon-location.svg"
+
+const Map = () => {
+    const ref = useRef();
+
+    useEffect(() => {
+        var map = window.L.map(ref.current);
+        map.backgroundColor = '#ff0000';
+
+        console.log('map', map);
+
+        map.setView([41.48169209345018, -71.31036492647061], 15);
+
+        window.L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 15,
+            minZoom: 1,
+            'center': [0, 0],
+            attribution: 'red',
+            style: myStyle
+        }).addTo(map);
+
+
+        var myStyle = {
+            "color": "orange",
+            "opacity": 0.65
+        };
+
+
+
+
+        // var marker = window.L.marker([41.48169209345018, -71.31036492647061], {
+        //     draggable: true,
+        //     title: "MODERN ART GALLERY"
+        // }).addTo(map).bindPopup("Hello");
+
+        var iconOptions = {
+            iconUrl: icon,
+            iconSize: [66,88]
+        }
+
+        var customIcon = window.L.icon(iconOptions);
+        var markerOptions = {
+            icon: customIcon,
+        }
+
+        var marker = window.L.marker([41.48169209345018, -71.31036492647061], markerOptions).addTo(map);
+
+
+
+        // var circle = window.L.circle([41.48169209345018, -71.31036492647061], {
+        //     color: 'orange',
+        //     // fillColor: '#f03',
+        //     fillOpacity: 0.5,
+        //     radius: 50
+        // }).addTo(map);
+
+        return () => {
+            console.log(map);
+        }
+
+    }, [ref])
+
+    return (
+        <div className={mapStyles.map__wrap}  ref={ref}>
+        </div>
+    );
+};
+
+export default Map;
